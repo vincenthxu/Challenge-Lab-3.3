fb6e760b4f5bb9a5a2fc216e06ef4f2db6d19036
Vincent Xu <vincent.xu1@outlook.com>
2025-07-21 13:48:06 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Challenge_Lab_3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DetermineTargetIndices([2, 7, 11, 15], 9));
+        }
+
+        static (int, int) DetermineTargetIndices(int[] ints, int target)
+        {
+            for (int i = 0; i < ints.Length; i++)
+            {
+                if (ints[i] < target)
+                {
+                    for (int j = i; j < ints.Length; j++)
+                    {
+                        if (target - ints[j] == ints[i])
+                            return (i, j);
+                    }
+                }
+            }
+            return (-1, -1);
+        }
+    }
+}
