6a221beb775d3fa0d863661201cab893f0410233
Vincent Xu <vincent.xu1@outlook.com>
2025-07-23 13:36:34 -0700

    Modified condition to allow for the integers 0 and the target itself to be valid addends

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@
         {
             for (int i = 0; i < ints.Length; i++)
             {
-                if (ints[i] < target)
+                if (ints[i] <= target)
                 {
                     for (int j = i; j < ints.Length; j++)
                     {
